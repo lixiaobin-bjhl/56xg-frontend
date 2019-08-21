684b4589826920104745ea13dc77c90998d71516
lixiaobin <lixiaobin8878@gmail.com>
2019-08-21 16:38:04 +0800

    typescript配置

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    env: {
+        browser: true,
+    },
+    globals: {
+        cc: false
+    },
+    extends: [
+        'eslint-config-alloy/typescript'
+    ],
+    // add your custom rules here
+    rules: {
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        // 取消分号
+        // 'semi': ['error', 'never'],
+        // 关闭强制使用"==="
+        'eqeqeq': 'off',
+        // 允许使用for-in
+        'guard-for-in': 'off',
+        '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'no-public' }],
+        '@typescript-eslint/explicit-function-return-type': [
+            'off', 
+            {
+                allowExpressions: true, 
+                allowTypedFunctionExpressions: true,
+            }
+        ],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/camelcase': ['off', {properties: 'always'}],
+        '@typescript-eslint/no-unused-vars': ['error', {
+            'vars': 'all',
+            'args': 'none',
+            'ignoreRestSiblings': true,
+        }],
+        "react-native/no-color-literals": 0,
+        "react-native/sort-styles": 0,
+        "react-native/no-inline-styles":0,
+    }
+}
--- assets/Script/components/start.ts
+++ assets/Script/components/start.ts
@@ -1,31 +1,18 @@
-// Learn TypeScript:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class StartClass extends cc.Component {
 
     @property(cc.Label)
     label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
-
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {}
-
-    start () {
-
+    onLoad() {
+        this.label.string = 'welcome 卡5星';
+        setTimeout(() => {
+            cc.director.loadScene('login');
+        }, 2000);
     }
 
+    start() {}
     // update (dt) {}
 }
